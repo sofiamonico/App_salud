7287f7f83a61ceb9852033ee9a394f839d44b590
Sofia Monico <sofiamonico02@gmail.com>
2022-11-09 15:24:03 -0300

    subiendo primer archivo

---
+++ src/main/java/com/salud/webSalud/WebSaludApplication.java
@@ -0,0 +1,13 @@
+package com.salud.webSalud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSaludApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSaludApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salud/webSalud/WebSaludApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salud.webSalud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSaludApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

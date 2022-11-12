f2e22572444e9ca7df03898eada107192888fd7b
Ema Trillini <ematrillini@gmail.com>
2022-11-12 18:50:32 -0300

    Query

--- src/main/java/com/salud/webSalud/persistence/repository/MedicoRepositorio.java
+++ src/main/java/com/salud/webSalud/persistence/repository/MedicoRepositorio.java
@@ -1,6 +1,17 @@
 package com.salud.webSalud.persistence.repository;
 import com.salud.webSalud.persistence.entity.Medico;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MedicoRepositorio extends JpaRepository<Medico,Integer> {
+    @Query("SELECT m FROM Medico m WHERE m.nombre = :nombre")
+    public Medico buscarPorNombre(@Param("nombre") String nombre);
+    
+    
+    @Query ("SELECT m FROM Medico m WHERE m.especialidad = :especialidad")
+    public List<Medico> buscarPorEspecialidad(@Param ("especialidad") String especialidad);
 }
